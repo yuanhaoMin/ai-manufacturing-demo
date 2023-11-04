f7f85c212fdc86ab715d75d78d507db6a88b04b3
Yuanhao Min (Shenzhen) <min.yuanhao.1997.11.12@outlook.com>
2023-11-04 14:19:20 +0800

    added steel_plates_fault notebook

---
+++ dependency.py
@@ -0,0 +1,12 @@
+# python -m venv C:\workspace\ai-manufacturing-demo\venv
+# .\venv\Scripts\activate
+# pip install -r requirements.txt
+
+# pip install pipreqs
+# pipreqs ./ --force --encoding=utf8
+import matplotlib
+import numpy
+import pandas
+import seaborn
+import sklearn
+import tabulate
